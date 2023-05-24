afe9eda0c5374644609f41e20c6b6232ac8cee5d
Malvin Valerian <malvinval09@gmail.com>
2023-05-24 21:41:29 +0700

    add: body content

--- src/App.js
+++ src/App.js
@@ -47,6 +47,23 @@ const App = () => {
             </div>
           </div>
         </div>
+
+        <div id="third-body-content" className="w-full flex flex-col-reverse lg:flex-row mt-14 justify-center">
+          <div className="w-full mt-10 lg:mt-0 h-full flex flex-col items-center lg:items-end justify-center">
+            <div id="first-body-title-container" className="w-3/4 lg:w-full">
+              <h1 className="font-garamond font-weight-800 text-4xl lg:text-6xl text-center lg:text-justify">We are open!</h1>
+            </div>
+
+            <div id="first-body-text-container" className="w-3/4 lg:w-full my-8">
+              <p className="font-garamond text-lg text-center lg:text-justify leading-7">
+                Neetfood is located in South Jakarta, Indonesia. Working hours for weekdays are <strong>09.00 to 22.00</strong>, while for weekends <strong>09.00 to 19.00</strong>. See you at Netfood!
+              </p>
+            </div>
+          </div>
+          <div className="w-full h-full flex items-center justify-center lg:justify-end">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29225.97464768894!2d106.7619106850658!3d-6.189024430448973!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f716e3eecd51%3A0xd946d87a48641634!2sLayar%20Seafood%20%26%20Ikan%20Bakar!5e0!3m2!1sid!2sid!4v1684938532731!5m2!1sid!2sid" className='w-full h-full lg:w-17/20' style={{"border" : "0"}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" title='neetfoof-loc'></iframe>
+          </div>
+        </div>
       </div>
       </div>
     </div>
