f355b40974234f5c75314fb5373d8bc46f061e6f
Malvin Valerian <malvinval09@gmail.com>
2023-06-01 13:52:03 +0700

    update: menu page

--- src/Pages/Menu.js
+++ src/Pages/Menu.js
@@ -1,16 +1,45 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "../components/ui/Navbar";
 import FoodIMG from "../components/ui/img/food2.png";
 import MenuList from "../data/menu.json";
 import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { AiOutlinePlus } from 'react-icons/ai';
+import { AiOutlineMinus } from 'react-icons/ai';
 import "../index.css";
 
 const Menu = () => {
     const MenuListArr = MenuList.data.menu;
-    const [filteredMenu, setFilteredMenu] = useState(MenuListArr[0])
+    const [filteredMenu, setFilteredMenu] = useState(MenuListArr[0]);
+    const [modalTrigerred, setModalTrigerred] = useState(false);
+    const [selectedMenuName, setSelectedMenuName] = useState("");
+    const [selectedMenuDesc, setSelectedMenuDesc] = useState("");
+    const [selectedMenuConstantPrice, setSelectedMenuConstantPrice] = useState(0);
+    const [selectedMenuPrice, setSelectedMenuPrice] = useState("");
+    const [selectedMenuQty, setSelectedMenuQty] = useState(1);
 
-    console.log(filteredMenu)
+    const handleAddToCart = (name, desc, price) => {
+        setModalTrigerred(!modalTrigerred);
+        setSelectedMenuName(name);
+        setSelectedMenuDesc(desc);
+        setSelectedMenuPrice(price);
+        setSelectedMenuConstantPrice(price);
+        setSelectedMenuQty(1);
+    }
 
+    const handleMenuCartQty = (operator) => {
+        if (operator === '+') {
+            setSelectedMenuQty(selectedMenuQty + 1)
+        } else {
+            if (selectedMenuQty > 1) {
+                setSelectedMenuQty((prevMenuQty) => {return prevMenuQty - 1})
+            }
+        }
+    }
+
+    useEffect(() => {
+        setSelectedMenuPrice(selectedMenuConstantPrice * selectedMenuQty)
+    }, [selectedMenuQty])
+    
     const element = (
         <div id="container" className="">
             <Navbar position="top" />
@@ -20,22 +49,22 @@ const Menu = () => {
                             <option value={0}>Pick a category</option>
                             {
                                 MenuListArr.map((m, index) => {
-                                    return <option value={index}>{m.title}</option>
+                                    return <option key={index} value={index}>{m.title}</option>
                                 })
                             }
                         </select>
                         
                         <div className="w-full flex flex-wrap justify-center lg:justify-between lg:mx-0 mb-20">
                             {
-                                filteredMenu.Dishes.map((d) => {
+                                filteredMenu.Dishes.map((d, index) => {
                                     return (
-                                        <div className="card font-garamond static card-compact w-72 bg-base-100 shadow-xl mx-2 lg:mx-0 my-5">
+                                        <div key={index} className="card font-garamond static card-compact w-72 bg-base-100 shadow-xl mx-2 lg:mx-0 my-5">
                                             <figure><img src={FoodIMG} alt="menu" /></figure>
                                             <div id="menu-card-body" className="card-body">
                                                 <h2 className="card-title">{d.name}</h2>
                                                 <p className="text-lg">{d.description}</p>
-                                                <div className="card-actions justify-end">
-                                                    <button id="add-to-cart-btn" className="btn btn-accent text-white">
+                                                <div className="flex card-actions justify-end">
+                                                    <button onClick={() => handleAddToCart(d.name, d.description, d.price)} id="add-to-cart-btn" className="btn btn-accent text-white">
                                                         <div className="text-lg flex items-center"><AiOutlineShoppingCart className="mr-2" /> ${d.price}</div>
                                                     </button>
                                                 </div>
@@ -45,6 +74,46 @@ const Menu = () => {
                                 })
                             }
                         </div>
+
+                        {
+                            modalTrigerred ? 
+                            <>
+                                <div className="font-garamond fixed inset-0 flex items-center justify-center z-50 font-poppins">
+                                    <div className="modal-box">
+                                        <div className="mt-5">
+                                            <div className="mt-3">
+                                                <p className="text-lg font-bold uppercase">{selectedMenuName}</p>
+                                            </div>
+
+                                            <div className="mt-3">
+                                                <label className="text-lg">{selectedMenuDesc}</label>
+                                            </div>
+
+                                            <div className="mt-5">
+                                                <div className="flex items-center">
+                                                    <p className="mr-5 font-bold text-lg">Quantity</p>
+                                                    <div onClick={() => handleMenuCartQty('-')} className=" bg-pink-600 text-white p-1 rounded-xl cursor-pointer"><AiOutlineMinus /></div>
+                                                    <span className="font-bold mx-10">{selectedMenuQty}</span>
+                                                    <div onClick={() => handleMenuCartQty('+')} className="bg-teal-600 text-white p-1 rounded-xl cursor-pointer">
+                                                        <AiOutlinePlus />
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div className="mt-5">
+                                                <label className="text-lg">Total: <strong>${selectedMenuPrice}</strong></label>
+                                            </div>
+                                        </div>
+                                        <div className="modal-action mt-8">
+                                            <label onClick={() => setModalTrigerred(false)} className="btn btn-error text-white">Cancel</label>
+                                            <label htmlFor="my-modal" className="btn">Add to cart</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </>
+                            :
+                            ""
+                        }
                     </div>
                 </div>
             <Navbar position="bottom" route="/menu" />
